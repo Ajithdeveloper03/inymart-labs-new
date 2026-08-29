80d6e0d2dedf98993d33b095982bd81f2bd38b3e
saravananpalanisamy2003 <saravananpalani>
2026-08-29 15:24:35 +0530

    correction

--- php-backend/update_pass.php
+++ php-backend/update_pass.php
@@ -1,12 +1,7 @@
 <?php
-$host = 'localhost';
-$db_name = 'inymart_blog';
-$username = 'root';
-$password = '';
+require_once 'api/db.php';
 
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$db_name;charset=utf8mb4", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
     $newHash = password_hash('inymartlabs@2026', PASSWORD_DEFAULT);
     
